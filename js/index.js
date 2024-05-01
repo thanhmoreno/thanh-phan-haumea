7ddd6c0de8fa6a5ca2a07b05dc6ff1512e243bff
Nancyy Ph <nancyyph@NancyMacPro.local>
2024-05-01 15:43:05 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -38,5 +38,63 @@ for ( let i = 0; i < arrayOfSkills.length; i++) {
         let skill = document.createElement("li");
         skill.textContent = arrayOfSkills[i];
         skillsList.appendChild(skill);
-}   
+} 
+
+// Create a variable named messageForm that uses "DOM Selection" to select the "leave_message" form by name attribute
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+// add EventListener to messageForm
+// add preventDefault form submission behavior
+messageForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+// create three new varibles and retrieve the value
+        const usersName = event.target.usersName.value;
+        const usersEmail = event.target.usersEmail.value;
+        const usersMessage = event.target.usersMessage.value;
+
+// add console.log statement 
+        console.log(usersName);
+        console.log(usersEmail);
+        console.log(usersMessage);
+
+// create variable named messageSection
+        const messageSection = document.querySelector('messages');
+
+// create variable named messageList to query messageSection
+        const messageList = messageSection.querySelector('ul');
+
+// create variable named newMessage make new list
+//
+        const newMessage = document.createElement("li");
+        newMessage.innerHTML = '<a href="mailto:' + usersEmail + '">' + usersName + '</a> <span>' + usersMessage + '</span>';
+
+// create variable removeButton make new button
+        const removeButton = document.createElement("button");
+
+// set type attribute to "button"
+        removeButton.type = "button";
+
+// set inner text to "remove"
+        removeButton.innerText = "remove";
+
+// add event listenser to removeButton
+        removeButton.addEventListener('click', function(event) {
+
+// create variable named entry
+            const entry = removeButton.parentNode; //parentNode property
+
+// add remove method
+            entry.remove();  
+        });
+
+// append removeButton to newMessage
+        newMessage.appendChild(removeButton);
+
+// append newMessage to messageList
+        messageList.appendChild(newMessage);
+
+// add a reset method
+        messageForm.reset();
+});
 
